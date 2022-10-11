66b8b805a4f456a40cfd2b55d3def6e2ed5ccced
geostojanov <gestojanov@gmail.com>
2022-10-11 11:46:25 +0200

    Lesson 76 - watch and watchEffect - Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@
     <input type="text" v-model="search">
     <p>Search term - {{ search }}</p>
     <div :key='name' v-for="name in namesFiltered">{{ name }}</div>
+    <button @click="handeClick">stop watching</button>
   </div>
 </template>
 
 <script>
 import { ref, computed } from '@vue/reactivity'
+import { watch, watchEffect } from '@vue/runtime-core'
 
 
 export default {
@@ -18,11 +20,24 @@ export default {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
     
+    const stopWatch = watch(search, () => {
+      console.log('watch functon ran.')
+    })
+    
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect function ran.')
+    })
+    
     const namesFiltered = computed(() => {
       return names.value.filter(name => name.includes(search.value))
     })
     
-    return { names, search, namesFiltered }
+    const handeClick = () => {
+      stopWatch();
+      stopEffect();
+    }
+    
+    return { names, search, namesFiltered, handeClick }
   }
 }
 </script>
