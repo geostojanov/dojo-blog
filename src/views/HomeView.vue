07d962abe8df2a6790b2af5609a63b2c5e4c80e8
geostojanov <gestojanov@gmail.com>
2022-10-12 08:54:40 +0200

    Lesson 80 - reusable composable functions - Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,31 +11,16 @@
 
 <script>
 import PostList from '../components/PostList.vue'
-import { ref } from '@vue/reactivity'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
     
-    const posts = ref([])
-    const error = ref(null)
+    const { posts, error, load } = getPosts()
     
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-    
-    load();
+    load()
     
     return { posts, error }
   }
