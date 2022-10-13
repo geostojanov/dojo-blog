81c6af50c923c726e4fb8fe9f10cd393e253f41a
geostojanov <gestojanov@gmail.com>
2022-10-13 09:31:40 +0200

    Lesson 82 - creating a loading spinner - Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,17 +5,18 @@
     <div v-if="posts.length">
       <PostList :posts="posts" />
     </div>
-    <div v-else>Loading...</div>
+    <div v-else><Spinner /></div>
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue'
 import getPosts from '../composables/getPosts'
+import Spinner from '../components/Spinner.vue'
 
 export default {
   name: 'HomeView',
-  components: { PostList },
+  components: { PostList, Spinner },
   setup() {
     
     const { posts, error, load } = getPosts()
