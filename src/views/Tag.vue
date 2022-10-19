e8d422e87543bb4ca835108a2e097992bc00f882
geostojanov <gestojanov@gmail.com>
2022-10-19 08:52:20 +0200

    Lesson 90 - creating a tag cloud - Done

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="tag">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="tagPosts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else><Spinner /></div>
   </div>
@@ -12,11 +13,12 @@
 import getPosts from '../composables/getPosts'
 import Spinner from '../components/Spinner.vue'
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 import { useRoute } from 'vue-router'
 import { computed } from '@vue/runtime-core'
 
 export default {
-  components: { Spinner, PostList },
+  components: { Spinner, TagCloud, PostList },
   setup() {
     const router = useRoute();
     
@@ -34,5 +36,9 @@ export default {
 </script>
 
 <style>
-
+  .tag {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
 </style>
